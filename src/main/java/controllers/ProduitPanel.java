dd103b6c4d7d5b0c3bce377c1936e772dd9b5a3a
yahya <yahya.bichiou@esprit.tn>
2025-04-21 01:46:51 +0100

    Correction Front

--- src/main/java/controllers/ProduitPanel.java
+++ src/main/java/controllers/ProduitPanel.java
@@ -2,6 +2,8 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -12,23 +14,53 @@ public class ProduitPanel {
     @FXML
     public Label prix;
     @FXML
-    public Label quantite;
+    public Spinner quantite;
     @FXML
     public ImageView image;
+    @FXML
+    public Label prixTotal;
+    String prixNum;
 
     public void setData(String nom, String prix, String quantite, String image) {
         this.nom.setText(nom);
-        this.prix.setText(prix);
-        this.quantite.setText(quantite);
+        this.prixNum = prix;
+        this.prix.setText(prixNum + " DT");
+
         try {
-            //Image image2 = new Image("https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png");
+            int quantiteValue = Integer.parseInt(quantite);
+            this.quantite.getValueFactory().setValue(quantiteValue);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid quantity: " + quantite);
+        }
+
+        try {
             this.image.setImage(new Image(image));
         } catch (Exception e) {
             System.out.println("Error loading image: " + image);
             e.printStackTrace();
         }
 
+        updatePrixTotal();
     }
 
+
+    private void updatePrixTotal() {
+        try {
+            int qte = (int) quantite.getValue();
+            double unitPrice = Double.parseDouble(prixNum);
+            double total = qte * unitPrice;
+            prixTotal.setText(String.format("%.2f", total) + " DT");
+        } catch (Exception e) {
+            prixTotal.setText("Error");
+        }
+    }
+
+
+
+    @FXML
+    public void initialize() {
+        quantite.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100, 1));
+        quantite.valueProperty().addListener((obs, oldValue, newValue) -> updatePrixTotal());
+    }
 
 }
