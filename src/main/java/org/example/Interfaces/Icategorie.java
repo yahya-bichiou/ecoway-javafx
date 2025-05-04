d547a1adbd8084dfbdc7e20ef51b5e5560be3498
yahya-bichiou <130942204+yahya-bichiou@users.noreply.github.com>
2025-05-04 22:46:48 +0100

    Delete file

--- src/main/java/org/example/Interfaces/Icategorie.java
+++
@@ -1,8 +0,0 @@
-package org.example.Interfaces;
-import java.util.List;
-public interface Icategorie <C>{
-    void addcategorie(C c);
-    void updatecategorie(C c);
-    void deletecategorie(int id);
-    List<C> getAll();
-}
